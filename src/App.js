3b32dc508293b7d749b4de9fada56d84c399b481
ShyloBreanne <sblyall@gmail.com>
2023-07-27 14:17:46 -0700

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-    </div>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+            <small>Coded by Breanne</small>
+          </footer>
+        </div>
+      </div>
   );
 }
 
