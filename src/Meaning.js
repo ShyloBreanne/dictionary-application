e27f2bea3fc5ce37e161bf36dd9e43942d3ddd8b
ShyloBreanne <sblyall@gmail.com>
2023-07-28 10:27:08 -0700

    Fixed format of Meaning

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,16 +5,17 @@ export default function Meaning(props) {
     return (
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
-            {props.meaning.definition.map(function (definition, index) {
-                return (
-                    <div key={index}>
-                        <p>
-                            {definition.definition}
-                            <br />
-                           <em>{definition.example}</em>
-                    </p>
-                    </div>
-                );
+            {props.meaning.definition.map(function (definition, index)
+            {
+                    return (
+                        <div key={index}>
+                            <p>
+                                {definition.definition}
+                                <br />
+                                <em>{definition.example}</em>
+                            </p>
+                        </div>
+                    );
                 })}
         </div>
     );
