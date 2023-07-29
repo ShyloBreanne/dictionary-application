31d145b2da8e0fe1b8bc9de5169c63852997a4c1
ShyloBreanne <sblyall@gmail.com>
2023-07-29 14:32:34 -0700

    Improved styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -26,7 +26,7 @@ export default function Dictionary(props) {
         let pexelsApiKey =
             "lsSBCbTYc4DrIJv8IsDsYDo9YYHFweH2KmOGhKW6ZmZmrsbbUqecBCnU";
         let pexelsApiUrl =
-            `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+            `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
         let headers = { "Authorization": ` ${pexelsApiKey}` };
         axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
         }
