8816c8f6a5ea507343bf0bab0fd0295c63091f9d
ShyloBreanne <sblyall@gmail.com>
2023-07-29 17:47:20 -0700

    fixed broken link

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
+          <a href="https://www.instagram.com/bre.codes/" target="_blank" rel="noreferrer">  
             <img src={logo} className="App-logo img-fluid" alt="logo" />
+            </a>
         </header>
         <main>
           <Dictionary defaultKeyword="meditate" />
         </main>
         <footer className="App-footer">
-            ✨This project was coded by <a href="www.linkedin.com/in/breannelyall" target="_blank" rel="noreferrer">Breanne Lyall</a> ✨ <a href="https://github.com/ShyloBreanne/dictionary-application" target="_blank" rel="noreferrer">Open-sourced on GitHub</a> and <a href="https://dictionary-app-brecodes.netlify.app" target="_blank" rel="noreferrer">hosted on Netlify</a>✨
+            ✨This project was coded by <a href="https://www.linkedin.com/in/breannelyall" target="_blank" rel="noreferrer">Breanne Lyall</a> ✨ <a href="https://github.com/ShyloBreanne/dictionary-application" target="_blank" rel="noreferrer">Open-sourced on GitHub</a> and <a href="https://dictionary-app-brecodes.netlify.app" target="_blank" rel="noreferrer">hosted on Netlify</a>✨
             <br />
           📷 <a href="https://www.pexels.com">Photos provided by Pexels</a> 📷
           </footer>
